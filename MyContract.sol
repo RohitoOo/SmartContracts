d213c77078ef6e4c7e82ce4ac2cd34a9159065ea
Rohito Bhambhani <bhambhani.ba@gmail.com>
2021-05-25 16:55:46 -0400

    Array Mapping

--- MyContract.sol
+++ MyContract.sol
@@ -43,3 +43,25 @@ contract MyContract {
     }
 }
 
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract MyContract {
+   Person[] public people;
+   
+   // Internal count of people 
+   // Using Array
+   
+   uint256 public peopleCount;
+   
+   struct Person {
+       string _firstName;
+       string _lastName;
+   }
+   
+   function addPerson(string memory _firstName, string memory _lastName) public {
+       people.push(Person(_firstName,_lastName));
+       peopleCount += 1;
+   }
+}
+
