e7b135390fd0d484ea8124e09c537c97bfc7a4fa
Rohito Bhambhani <bhambhani.ba@gmail.com>
2021-05-25 17:17:30 -0400

    Internal Function

--- MyContract.sol
+++ MyContract.sol
@@ -90,3 +90,29 @@ contract MyContract {
    }
 }
 
+pragma solidity >=0.7.0 <0.9.0;
+
+contract MyContract {
+    
+    // Using Mapping - Similar to Hash Table - Reference by Key Value Pair 
+
+    uint256 public peopleCount = 0;
+
+    mapping(uint => Person) public people;
+   
+   struct Person {
+       uint _id;
+       string _firstName;
+       string _lastName;
+   }
+   
+   function addPerson(string memory _firstName, string memory _lastName) public {
+       incrementCount();
+        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+   }
+   
+   function incrementCount() internal {
+       peopleCount += 1;
+   }
+}
+
